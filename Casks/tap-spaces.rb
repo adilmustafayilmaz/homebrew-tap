81939263ace067c07029e78b984a3b5cb1bef9d4
Adil Mustafa Yılmaz <79438419+adilmustafayilmaz@users.noreply.github.com>
2026-07-18 14:55:13 +0300

    Update tap-spaces to 0.2.0

--- Casks/tap-spaces.rb
+++ Casks/tap-spaces.rb
@@ -1,6 +1,6 @@
 cask "tap-spaces" do
-  version "0.1.0"
-  sha256 "dae977be1ce7c3351e292f5fc0588d7dc614be0363fb2ce9035b4b47ec61d49e"
+  version "0.2.0"
+  sha256 "5dfc9b45d720c6e3627d22ca4bc47dad81c2c0c438038d5d47df9b6fe566c26d"
 
   url "https://github.com/adilmustafayilmaz/Tap-Spaces/releases/download/v#{version}/TapSpaces-#{version}.zip"
   name "Tap Spaces"
