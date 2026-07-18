f98499c8b25ed4d9667f6f3a29182d24f34c6a31
Adil Mustafa Yılmaz <79438419+adilmustafayilmaz@users.noreply.github.com>
2026-07-18 13:51:49 +0300

    Add tap-spaces cask 0.1.0

---
+++ Casks/tap-spaces.rb
@@ -0,0 +1,19 @@
+cask "tap-spaces" do
+  version "0.1.0"
+  sha256 "dae977be1ce7c3351e292f5fc0588d7dc614be0363fb2ce9035b4b47ec61d49e"
+
+  url "https://github.com/adilmustafayilmaz/Tap-Spaces/releases/download/v#{version}/TapSpaces-#{version}.zip"
+  name "Tap Spaces"
+  desc "Detects which zone of the desk you tapped and fires a keyboard shortcut"
+  homepage "https://github.com/adilmustafayilmaz/Tap-Spaces"
+
+  depends_on macos: ">= :sonoma"
+
+  app "TapSpaces.app"
+
+  uninstall quit: "com.mustafa.tapspaces"
+
+  zap trash: [
+    "~/Library/Application Support/TapSpaces",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-tap
+
+Homebrew tap for [Tap Spaces](https://github.com/adilmustafayilmaz/Tap-Spaces).
+
+```bash
+brew tap adilmustafayilmaz/tap
+brew install --cask tap-spaces
+```
